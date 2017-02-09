2ba1e4c0ede11e311d51845cbd9e137dbef637cb
Alexandre Barachant <alexandre.barachant@gmail.com>
2017-02-09 16:50:26 +0100

    rename files

--- lsl-record.py
+++ lsl-record.py
@@ -6,7 +6,7 @@ from optparse import OptionParser
 from pylsl import StreamInlet, resolve_byprop
 from sklearn.linear_model import LinearRegression
 
-default_fname = ("data_%s.csv" % strftime("%Y-%m-%d-%H:%M:%S", gmtime()))
+default_fname = ("data_%s.csv" % strftime("%Y-%m-%d-%H.%M.%S", gmtime()))
 parser = OptionParser()
 parser.add_option("-d", "--duration",
                   dest="duration", type='int', default=60,
