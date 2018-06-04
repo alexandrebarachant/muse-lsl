c07ed43195a16ce5d3b9f7900a4553105a6532e2
kowalej <kowaleskij@gmail.com>
2018-06-04 18:07:15 -0400

    Fixed helpers.resolve_backend function to properly check for Windows version. If version meets requirements to run BlueMuse, it becomes the default backend. Still falls back to bgapi and defaults to gatt on Linux.

--- muselsl/helper.py
+++ muselsl/helper.py
@@ -1,4 +1,4 @@
-from sys import platform
+import platform
 import warnings
 
 def warn_bluemuse_not_supported():
@@ -6,9 +6,12 @@ def warn_bluemuse_not_supported():
 
 def resolve_backend(backend):
     if backend in ['auto', 'gatt', 'bgapi', 'bluemuse']:
+        platformName = platform.system().lower()
         if backend == 'auto':
-            if platform == "linux" or platform == "linux2":
+            if platformName == 'linux' or platformName == 'linux2':
                 backend = 'gatt'
+            elif platformName == 'windows' and int(platform.version().replace('.','')) >= 10015063:
+                backend = 'bluemuse'
             else:
                 backend = 'bgapi'
         return backend
