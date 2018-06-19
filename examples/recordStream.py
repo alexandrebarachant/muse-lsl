ca36c4167b48c6c3b2363215d5ba9af25a59830b
jdpigeon <morrisondano@gmail.com>
2018-06-19 19:24:59 -0400

    Added relaxation example that prints out band powers in console

--- examples/recordStream.py
+++ examples/recordStream.py
@@ -1,7 +1,14 @@
+"""
+Record from a Stream
+
+This example shows how to record data from an existing Muse LSL stream
+"""
 from muselsl import record
 
-# Note: an existing Muse LSL stream is required
-record(60)
+if __name__ == "__main__":
+    
+    # Note: an existing Muse LSL stream is required
+    record(60)
 
-# Note: Recording is synchronous, so code here will not execute until the stream has been closed
-print('Recording has ended')
+    # Note: Recording is synchronous, so code here will not execute until the stream has been closed
+    print('Recording has ended')
--- examples/startMuseStream.py
+++ examples/startMuseStream.py
@@ -1,11 +1,19 @@
+"""
+Starting a Stream
+
+This example shows how to search for available Muses and
+create a new stream
+"""
 from muselsl import stream, list_muses
+
+if __name__ == "__main__":
 
 muses = list_muses()
 
-if not muses:
-    print('No Muses found')
-else:
-    stream(muses[0]['address'])
+    if not muses:
+        print('No Muses found')
+    else:
+        stream(muses[0]['address'])
 
-    # Note: Streaming is synchronous, so code here will not execute until the stream has been closed
-    print('Stream has ended')
+        # Note: Streaming is synchronous, so code here will not execute until the stream has been closed
+        print('Stream has ended')
