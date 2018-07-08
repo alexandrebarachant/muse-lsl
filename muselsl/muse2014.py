94a2cf5720e01b35d8b215fe64ff98a64947aca8
khoo-jeremy <jperson39@gmail.com>
2018-07-08 15:14:40 -0700

    Make spelling, formatting, and convention corrections

--- muselsl/muse2014.py
+++ muselsl/muse2014.py
@@ -5,7 +5,8 @@ import numpy
 from pythonosc import dispatcher
 from pythonosc import osc_server
 
-class Muse_2014():
+
+class Muse2014:
     """
     Function: Provides helper functions for Muse 2014 headband
 
@@ -21,8 +22,7 @@ class Muse_2014():
     - eeg_handler: Timestamps and transforms the data
     """
 
-
-    def __init__(self, ip="127.0.0.1", port=5000, time_func = time.time, callback_eeg=None):
+    def __init__(self, ip="127.0.0.1", port=5000, time_func=time.time, callback_eeg=None):
         """
         Function: Initializes muse 2014
         Args:
@@ -32,37 +32,35 @@ class Muse_2014():
         Returns: None
         """
         print("Initializing Muse 2014 Devices")
-        #Device is initially not connected
+        # Device is initially not connected
         self.connected = False
-        #Initialize port number and ip
+        # Initialize port number and ip
         self.ip = ip
         self.port = port
-        #Initialize server
+        # Initialize server
         self.server = None
-        #Initialize timestamp array
+        # Initialize timestamp array
         self.timestamps = []
-        #Intialize time fucntion and callback_eeg
+        # Initialize time function and callback_eeg
         self.time_func = time_func
         self.callback_eeg = callback_eeg
-        #Intialize data
+        # Initialize data
         self.data = []
-        #Initailize a dispatcher
+        # Initialize a dispatcher
         self._dispatcher = dispatcher.Dispatcher()
         self._dispatcher.map("/debug", print)
         self._dispatcher.map("/muse/eeg", self.eeg_handler,self)
-
 
     def connect(self):
         """
         Function: Connect to the Muse headset, but no streaming is done yet
         Returns: None
         """
-        if(self.connected == True): print("Sorry, headset already connected")
+        if self.connected: print("Sorry, headset already connected")
         else:
             self.server = osc_server.ThreadingOSCUDPServer(
                     (self.ip, self.port), self._dispatcher)
             print("Serving on {}".format(self.server.server_address))
-
 
     def start(self):
         """
@@ -72,9 +70,9 @@ class Muse_2014():
         print("Starting to stream data\n")
         try:
             self.server.serve_forever()
-        except KeyboardInterrupt: #Stops streaming on keyboard interrupt
+        except KeyboardInterrupt:
+            # Stops streaming on keyboard interrupt
             self.stop()
-
 
     def stop(self):
         """
@@ -82,8 +80,8 @@ class Muse_2014():
                   This means that you can start streaming data again using the start method
         Returns: None
         """
-        self.server.shutdown() #Stops Streaming the data
-
+        # Stops Streaming the data
+        self.server.shutdown()
 
     def resume(self):
         """
@@ -92,8 +90,7 @@ class Muse_2014():
         """
         self.start()
 
-
-    def eeg_handler(self,unusedAddr, args, ch1, ch2, ch3, ch4):
+    def eeg_handler(self, unusedAddr, args, ch1, ch2, ch3, ch4):
         """
         Function: Does real time timestamping. Any other minor data manipulation must be done here
         Args:
@@ -106,9 +103,9 @@ class Muse_2014():
 
 
 """ FOR TESTING PURPOSES ONLY """
-#Please uncomment this part if you want to test the code
-#Trigger a keyboard interrup if you would like to stop the code
+# Please uncomment this part if you want to test the code
+# Trigger a keyboard interrupt if you would like to stop the code
 # if __name__ == "__main__":
-#     muse_2014 = Muse_2014()
+#     muse_2014 = Muse2014()
 #     muse_2014.connect()
 #     muse_2014.start()
