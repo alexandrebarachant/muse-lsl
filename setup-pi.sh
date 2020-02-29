f1d03bce4abfbed4264dad5f9b531460006b9d72
felix <sirexeclp@gmail.com>
2020-02-29 14:24:38 +0100

    + dont clone repo for now

--- setup-pi.sh
+++ setup-pi.sh
@@ -3,5 +3,5 @@ sudo apt-get install -y python3-pip libatlas-base-dev git
 sudo pip3 install muselsl
 wget https://github.com/sirexeclp/muse-lsl/releases/download/1/liblsl32.so
 sudo cp liblsl32.so /usr/local/lib/python3.7/dist-packages/pylsl/liblsl32.so
-git clone https://github.com/sirexeclp/muse-lsl.git
-cd muse-lsl && git checkout feature/experiment
+#git clone https://github.com/sirexeclp/muse-lsl.git
+#cd muse-lsl && git checkout feature/experiment
