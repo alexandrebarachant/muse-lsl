b692de78043efd4dc70a1d8eac08e1639684c10c
felix <sirexeclp@gmail.com>
2020-02-29 14:23:09 +0100

    + clone repo w/ experiment code

--- setup-pi.sh
+++ setup-pi.sh
@@ -1,5 +1,7 @@
 sudo apt-get update
-sudo apt-get install -y python3-pip libatlas-base-dev
+sudo apt-get install -y python3-pip libatlas-base-dev git
 sudo pip3 install muselsl
 wget https://github.com/sirexeclp/muse-lsl/releases/download/1/liblsl32.so
 sudo cp liblsl32.so /usr/local/lib/python3.7/dist-packages/pylsl/liblsl32.so
+git clone https://github.com/sirexeclp/muse-lsl.git
+cd muse-lsl && git checkout feature/experiment
