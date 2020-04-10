9b8b0512b30b1f375c998946ed0cfce11a5b79ec
felix <sirexeclp@gmail.com>
2020-04-10 15:56:59 +0200

    format file

--- muselsl/run_experiment.py
+++ muselsl/run_experiment.py
@@ -9,33 +9,37 @@ import csv
 
 muse_macs = {
     "4EB2": "00:55:DA:B7:4E:B2"
-    ,"528C": "00:55:DA:B5:52:8C"
-    ,"528F": "00:55:DA:B5:52:8F"
+    , "528C": "00:55:DA:B5:52:8C"
+    , "528F": "00:55:DA:B5:52:8F"
 }
 
-NUM_PARTICIPANTS_PER_TRIAL = 2
+NUM_PARTICIPANTS_PER_TRIAL = 1
 
-ALL_DATATYPES=["EEG","PPG","ACC","GYRO"]
+ALL_DATATYPES = ["EEG", "PPG", "ACC", "GYRO"]
 
-muselsl_cli =  ["python3", "-m", "muselsl.__main__"]
+muselsl_cli = ["python3", "-m", "muselsl.__main__"]
 
-class SubProcess():
+
+class SubProcess:
     def __init__(self, target, kwargs, abort_event, name=None):
-        self.process = Process(target=target, kwargs=kwargs,name=name)
+        self.process = Process(target=target, kwargs=kwargs, name=name)
         self.abort_event = abort_event
         self.name = name
-    
+
     def start(self):
         self.process.start()
 
     def abort(self):
         self.abort_event.set()
-    
+
     def join(self):
         return self.process.join()
 
+
 class Participant:
     def __init__(self, experimental_run, participant_id, muse_id=None, name=None):
+        self.streaming_queue_rx = Queue()
+        self.streaming_queue_tx = Queue()
         self.experimental_run = experimental_run
         self.participant_id = participant_id
         self.muse_id = muse_id
@@ -45,7 +49,7 @@ class Participant:
         self.recording_procs = {}
 
     def get_input(self):
-        #self.name = input("Name: ")
+        # self.name = input("Name: ")
         while self.muse_mac is None:
             self.muse_id = input(f"[{self.participant_id}]Muse-ID: MUSE-").upper()
             self.muse_mac = muse_macs.get(self.muse_id, None)
@@ -53,18 +57,16 @@ class Participant:
 
     def start_streaming(self):
         from . import stream
-        self.streaming_queue_tx = Queue()
-        self.streaming_queue_rx = Queue()
-        self.streaming_proc = Process(target =stream, name="streaming_proc" ,kwargs={
-                "address":self.muse_mac, "ppg_enabled":True,
-                "acc_enabled":True, "gyro_enabled":True
-                ,"message_set": self.streaming_queue_rx
-                ,"message_get": self.streaming_queue_tx})
+        self.streaming_proc = Process(target=stream, name="streaming_proc", kwargs={
+            "address": self.muse_mac, "ppg_enabled": True,
+            "acc_enabled": True, "gyro_enabled": True
+            , "message_set": self.streaming_queue_rx
+            , "message_get": self.streaming_queue_tx})
         self.streaming_proc.start()
         while self.streaming_queue_rx.empty():
             sleep(0.5)
         return self.streaming_queue_rx.get() == StreamProcMessage.Started
-    
+
     def stop_streaming(self, join=True):
         self.streaming_queue_tx.put(StreamProcMessage.Aborting)
         if join:
@@ -73,26 +75,26 @@ class Participant:
     def _start_recording(self, filename, dejitter, data_type):
         from . import record
         e = Event()
-        p = SubProcess(target=record, abort_event=e,name=f"{data_type}_recording_proc"
-                ,kwargs={
-                "filename":filename
-                ,"data_source":data_type
-                ,"abort":e
-                ,"source_id": self.muse_mac
+        p = SubProcess(target=record, abort_event=e, name=f"{data_type}_recording_proc"
+                       , kwargs={
+                "filename": filename
+                , "data_source": data_type
+                , "abort": e
+                , "source_id": self.muse_mac
             })
         self.recording_procs[data_type] = p
         p.start()
-    
+
     def _stop_recording(self, data_type, join=True):
         p = self.recording_procs.pop(data_type, None)
         p.abort()
         if join:
             p.join()
-    
+
     def record_all(self, dejitter, data_types):
-        data_path = Path(self.experimental_run.data_root)  / Path(f"part{self.participant_id}")
+        data_path = Path(self.experimental_run.data_root) / Path(f"part{self.participant_id}")
         for t in data_types:
-            self._start_recording(data_path / f"{t}.csv",dejitter,t)
+            self._start_recording(data_path / f"{t}.csv", dejitter, t)
 
     def stop_all_recordings(self, join=True):
         for p in self.recording_procs.values():
@@ -104,8 +106,9 @@ class Participant:
 
         self.recording_procs = {}
 
+
 class ExperimentalRun:
-    def __init__(self, data_root ,num_participants, trial_id =None):
+    def __init__(self, data_root, num_participants, trial_id=None):
         self.num_participants = num_participants
         self.participants = []
         if trial_id is None:
@@ -114,21 +117,21 @@ class ExperimentalRun:
         self.data_root = Path(data_root) / Path(f"trial{self.trial_id}")
         self.data_root.mkdir(parents=True, exist_ok=False)
         self.markers_path = self.data_root / "markers.csv"
-        
+
         markers_file = self.markers_path.open("w")
         fieldnames = ['timestamp', 'tag']
         self.markers = csv.DictWriter(markers_file, fieldnames)
         self.markers.writeheader()
-    
+
     def add_marker(self, tag):
-        self.markers.writerow({"timestamp":datetime.now().isoformat(), "tag": tag})
+        self.markers.writerow({"timestamp": datetime.now().isoformat(), "tag": tag})
 
     def start(self):
         for i in range(self.num_participants):
             tmp = Participant(self, i)
             tmp.get_input()
             self.participants.append(tmp)
-        
+
         # for p in self.participants:
         #     success = False
         #     while not success:
@@ -136,10 +139,9 @@ class ExperimentalRun:
         #         success = p.start_streaming()
         #         print(f"{'Success' if success else 'Failed'} Started streaming of Part. {p.participant_id}")
 
-
         for p in self.participants:
             p.record_all(dejitter=True, data_types=ALL_DATATYPES)
-       
+
         print("all recording")
         self.add_marker("recording_started")
         while True:
@@ -150,22 +152,23 @@ class ExperimentalRun:
                 #         break
                 tag = input("Marker:")
                 self.add_marker(tag)
-                #sleep(1)
+                # sleep(1)
             except KeyboardInterrupt:
                 self.add_marker("KeyboardInterrupt")
                 break
-
 
         for p in self.participants:
             print("stopping recordings")
             p.stop_all_recordings()
             # print("stopping stream")
             # p.stop_streaming()
-        
+
         print("recordings stopped")
         self.add_marker("recordings stopped")
         # for p in self.participants:
         #     p.streaming_proc.join()
 
-run = ExperimentalRun(data_root="recordings", num_participants=NUM_PARTICIPANTS_PER_TRIAL)
-run.start()
+
+if __name__ == '__main__':
+    run = ExperimentalRun(data_root="recordings", num_participants=NUM_PARTICIPANTS_PER_TRIAL)
+    run.start()
