e0e7d6984585825306d9b57a212277791ac1744a
xloem <0xloem@gmail.com>
2022-03-09 17:34:08 -0500

    Default to the bleak backend.

--- muselsl/helper.py
+++ muselsl/helper.py
@@ -8,15 +8,18 @@ def warn_bluemuse_not_supported(extra_text = ''):
 
 
 def resolve_backend(backend):
-    if backend in ['auto', 'gatt', 'bgapi', 'bluemuse', 'bleak']:
-        platformName = platform.system().lower()
-        if backend == 'auto':
-            if platformName == 'linux' or platformName == 'linux2':
-                backend = 'gatt'
-            elif platformName == 'windows' and int(platform.version().replace('.', '')) >= 10015063:
-                backend = 'bluemuse'
-            else:
-                backend = 'bgapi'
+    if backend == 'auto':
+        ## if there are any issues with bleak,
+        ## below are the previous defaults
+        # platformName = platform.system().lower()
+        # if platformName == 'linux' or platformName == 'linux2':
+        #     backend = 'gatt'
+        # elif platformName == 'windows' and int(platform.version().replace('.', '')) >= 10015063:
+        #     backend = 'bluemuse'
+        # else:
+        #     backend = 'bgapi'
+        backend = 'bleak'
+    if backend in ['gatt', 'bgapi', 'bluemuse', 'bleak']:
         return backend
     else:
         raise(ValueError('Backend must be one of: auto, gatt, bgapi, bluemuse, bleak.'))
