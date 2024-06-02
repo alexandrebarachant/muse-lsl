7374c10978766f3333f607e5af2f23e4bca9d080
Dominique Makowski <dom.mak19@gmail.com>
2024-06-02 18:17:30 +0200

    Expose time_func to stream() and allow option to use pylsl timestamps (#197)
    * add --lsltime argument
    
    * add dest
    
    * it's dest or default
    
    * fix
    
    * Update stream.py

--- muselsl/cli.py
+++ muselsl/cli.py
@@ -98,13 +98,21 @@ class CLI:
             dest='disable_light',
             action='store_true',
             help='Turn off light on the Muse S headband')
+        parser.add_argument(
+            "-lslt",
+            "--lsltime",
+            default=False,
+            dest='lsl_time',
+            action="store_true",
+            help="Use pylsl's local_clock() for timestamps instead of Python's time.time()")
 
 
         args = parser.parse_args(sys.argv[2:])
         from . import stream
 
         stream(args.address, args.backend, args.interface, args.name, args.ppg,
-               args.acc, args.gyro, args.disable_eeg, args.preset, args.disable_light)
+               args.acc, args.gyro, args.disable_eeg, args.preset, args.disable_light,
+               args.lsl_time)
 
     def record(self):
         parser = argparse.ArgumentParser(
